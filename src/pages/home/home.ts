ca0e70fcdd5fea954e242f64cef3bc2953458102
BramBrouwer <bbrouwer040@gmail.com>
2017-10-04 16:36:05 +0200

    Revert "subejct"
    This reverts commit 5a8ef73586ceab8856987df2da666bd318d7464b.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,8 +5,6 @@ import { BleScanResponse } from "../../models/bleScanResponse";
 import { BLE } from '@ionic-native/ble';
 import { BeaconService } from "../../services/beacons";
 import { BeaconDetailPage } from "../beacon-detail/beacon-detail";
-import { ScanService } from "../../services/scan";
-import { Subject } from "rxjs/Subject";
 
 
 @Component({
@@ -18,12 +16,11 @@ export class HomePage {
   foundDevices: Array<BleScanResponse>;
   zone: NgZone;
   testBeacon: Beacon;
-  subject: Subject<Array<BleScanResponse>> = new Subject();
+
   constructor(
     public navCtrl: NavController, 
     public ble: BLE,
-    private beaconService: BeaconService,
-    private scanService: ScanService) {
+    private beaconService: BeaconService) {
     this.zone = new NgZone({ enableLongStackTrace: false });
     this.foundDevices = [];
     this.scanForBeacons();
@@ -33,20 +30,15 @@ export class HomePage {
     Scan for beacons. Called on page start&page refresh
   */
   scanForBeacons(){
-    this.scanService.scanForBeacons();
-    this.subject.subscribe(value => {
-      this.foundDevices = value;
+    this.ble.scan([], 10).subscribe(device => {
+      this.zone.run(() => {
+        var adData = new Uint8Array(device.advertising);
+        device.advertising = adData;
+        if(device.name){
+          this.foundDevices.push(device);          
+        }
+      })
     })
-    // this.ble.scan([], 10).subscribe(device => {
-    //   this.zone.run(() => {
-    //     var adData = new Uint8Array(device.advertising);
-    //     device.advertising = adData;
-    //     if(device.name){
-    //       this.foundDevices.push(device);          
-    //     }
-    //   })
-    // })
-
   }
 
    /*
